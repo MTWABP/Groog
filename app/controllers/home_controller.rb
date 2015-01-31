12be80686e34a8b7baac7ad69ea487f847305f61
Lewis Alva <lalva@tigertext.com>
2015-01-31 00:50:51 -0800

    Enable automatic update of groog.me

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+  protect_from_forgery except: [:ghpull]
+  skip_before_action :verify_authenticity_token, only: [:ghpull]
   def index
+  end
+  
+  def ghpull
+    # TODO: make tests and run them...lets be real, I'll never do this.
+    # email me so I can make sure it works manually
+    if `whoami` == "root\n"
+      `cd /home/groog/domains/groog.me/Groog`
+      `git stash`
+      `git pull`
+      `bundle`
+      `touch tmp/restart.txt`
+    end
+    render status: 200, json: {success: true}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  
+  post 'ghpull', to: 'home#ghpull'
+  
   mount Upmin::Engine => '/admin'
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   
