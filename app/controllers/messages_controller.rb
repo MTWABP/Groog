447b1454e3668ff8f9fbec7aeea29a21171724e1
root <root@localhost.localdomain>
2015-02-13 18:29:17 -0800

    Created pusher thanks Lewis

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,67 +1,36 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_group
-  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_message, only: [:destroy]
 
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.all
-  end
-
-  # GET /messages/1
-  # GET /messages/1.json
-  def show
-  end
-
-  # GET /messages/new
-  def new
-    @message = Message.new
-  end
-
-  # GET /messages/1/edit
-  def edit
+    @messages = @group.messages
+	
   end
 
   # POST /messages
   # POST /messages.json
   def create
-	@message = Message.create!(params[:message])
-    @message = Message.new(message_params)
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /messages/1
-  # PATCH/PUT /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+	@message = @group.messages.build(message_params)
+	@message.channel_type = "Group"
+	@message.channel_id = @group.slug
+	@message.user_id = current_user.id
+	
+	if @message.save
+		Pusher.trigger(@groups.class.to_s+'-'+@group.slug, 'new-message', {author: @message.user.to_s, created_at: @message.created_at, message: @message.body})
+		render json: @message
+	else 
+		render json: @message.errors
+	end
   end
 
   # DELETE /messages/1
   # DELETE /messages/1.json
   def destroy
     @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+	render json: "{hello: 'world'}"
   end
 
   private
